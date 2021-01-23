ee2d02c530384d5ee29134b4aeff33b2457d66af
StevenDRiggs <sfriggs21@gmail.com>
2021-01-23 04:42:57 -0700

    added build cache; moved debugger

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
-  debugger
   return (
     <div className={styles.container}>
       <Head>
