f2e9be453bbc898da17658c5987fca75b344c78c
StevenDRiggs <sfriggs21@gmail.com>
2021-01-23 04:30:03 -0700

    moved <script> tag in index.js into <Head>

--- pages/index.js
+++ pages/index.js
@@ -14,6 +14,8 @@ export default function Home() {
         <link href="https://fonts.googleapis.com/css2?family=Fraunces:wght@300&display=swap" rel="stylesheet" /> 
         
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous" />
+
+        <script type="text/javascript" src="/indexProcessing.js"></script>
       </Head>
 
       <header>
@@ -30,8 +32,6 @@ export default function Home() {
           <button type="submit" id="calculate">Calculate</button>
         </form>
       </main>
-
-      <script type="text/javascript" src="/indexProcessing.js"></script>
     </div>
   )
 }
